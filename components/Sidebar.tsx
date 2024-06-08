232614dd152144ed581b74e671bbcbe9c69fff60
“Anhunghezo” <anhkieu303252@gmail.com>
2024-06-08 20:21:45 +0700

    chore: Refactor layout and sidebar components

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,7 +8,7 @@ import {
   CommandList,
   CommandSeparator,
   CommandShortcut,
-} from '@/components/ui/command';
+} from "@/components/ui/command";
 import {
   LayoutDashboard,
   Newspaper,
@@ -16,45 +16,62 @@ import {
   CreditCard,
   Settings,
   User,
-} from 'lucide-react';
-import Link from 'next/link';
+} from "lucide-react";
+import Link from "next/link";
 
 const Sidebar = () => {
   return (
-    <Command className='bg-secondary rounded-none'>
-      <CommandInput placeholder='Type a command or search...' />
+    <Command className="bg-secondary rounded-none ">
       <CommandList>
-        <CommandEmpty>No results found.</CommandEmpty>
-        <CommandGroup heading='Suggestions'>
-          <CommandItem>
-            <LayoutDashboard className='mr-2 h-4 w-4' />
-            <Link href='/'>Dashboard</Link>
+        <CommandGroup>
+          <CommandItem
+            className="
+            text-primary-100
+            text-lg
+            flex
+            items-center
+            justify-start
+            hover:bg-primary-500
+          "
+          >
+            <LayoutDashboard className="mr-2 h-6 w-6" />
+            <Link href="/">Dashboard</Link>
           </CommandItem>
-          <CommandItem>
-            <Newspaper className='mr-2 h-4 w-4' />
-            <Link href='/posts'>Posts</Link>
+
+          <CommandItem
+            className="
+            text-primary-100
+            text-lg
+            flex
+            items-center
+            justify-start"
+          >
+            <User className="mr-2 h-6 w-6" />
+            <Link href="/users">Users</Link>
           </CommandItem>
-          <CommandItem>
-            <Folders className='mr-2 h-4 w-4' />
-            <Link href='#'>Categories</Link>
+
+          <CommandItem
+            className="
+            text-primary-100
+            text-lg
+            flex
+            items-center
+            justify-start"
+          >
+            <Newspaper className="mr-2 h-6 w-6" />
+            <Link href="/posts">Posts</Link>
           </CommandItem>
-        </CommandGroup>
-        <CommandSeparator />
-        <CommandGroup heading='Settings'>
-          <CommandItem>
-            <User className='mr-2 h-4 w-4' />
-            <span>Profile</span>
-            <CommandShortcut>⌘P</CommandShortcut>
-          </CommandItem>
-          <CommandItem>
-            <CreditCard className='mr-2 h-4 w-4' />
-            <span>Billing</span>
-            <CommandShortcut>⌘B</CommandShortcut>
-          </CommandItem>
-          <CommandItem>
-            <Settings className='mr-2 h-4 w-4' />
-            <span>Settings</span>
-            <CommandShortcut>⌘S</CommandShortcut>
+
+          <CommandItem
+            className="
+            text-primary-100
+            text-lg
+            flex
+            items-center
+            justify-start"
+          >
+            <Folders className="mr-2 h-6 w-6" />
+            <Link href="/reports">Reports</Link>
           </CommandItem>
         </CommandGroup>
       </CommandList>
